caf75f8f23079e240092c135ebcba5f4f1f0f74b
Parker Bacall <pbacall2@gmail.com>
2019-11-09 02:01:02 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -8,8 +8,19 @@ def turtles
     ]
 end
 
+def turtles
+    [
+      {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
+      {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
+    ]
+end
+
 def turtle_traits(turtles)
-  turtles.each do |turtle|
-    pp turtle[0][:traits]
+  turtles.map do |turtle|
+    turtle[:traits].each do |trait|
+      trait
+    end
   end
 end
