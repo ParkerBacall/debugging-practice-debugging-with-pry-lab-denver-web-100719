c07901f49b3560263b08e4c879a9bc56f8bc8f1b
Parker Bacall <pbacall2@gmail.com>
2019-11-09 02:07:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -10,7 +10,7 @@ end
 def turtle_traits(turtles)
   turtles.each do |turtle|
     turtle.each do |trait, key|
-      if trait == "traits"
+      if trait == traits
         key
         binding.pry
       end
